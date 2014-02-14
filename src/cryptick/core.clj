2d8ebeb15fd870e4a14d3e9a346beaf99f803d95
Reid McKenzie <rmckenzie92@gmail.com>
2014-02-14 04:10:20 -0600

    Initial whitespace cleanup

--- src/cryptick/core.clj
+++ src/cryptick/core.clj
@@ -15,42 +15,42 @@
                     :keepalive -1
                     }))
 
-(def feeds (atom {:btce {:url "https://btc-e.com/api/2" 
+(def feeds (atom {:btce {:url "https://btc-e.com/api/2"
                          :method :get
                          :pair-required? true
                          :pair-example "btc_usd"}
-                  :bter {:url "http://data.bter.com/api/1/ticker" 
+                  :bter {:url "http://data.bter.com/api/1/ticker"
                          :method :get
                          :pair-required? true
                          :pair-example "doge_btc"}
-                  :havelock {:url "https://www.havelockinvestments.com/r/tickerfull" 
+                  :havelock {:url "https://www.havelockinvestments.com/r/tickerfull"
                              :method :post}
-                  :bitstamp {:url "https://www.bitstamp.net/api/ticker/" 
+                  :bitstamp {:url "https://www.bitstamp.net/api/ticker/"
                              :method :get}
-                  :okcoin {:url "https://www.okcoin.com/api/ticker.do?symbol=" 
+                  :okcoin {:url "https://www.okcoin.com/api/ticker.do?symbol="
                            :method :get
                            :pair-required? true
                            :pair-example "ltc_cny"}
-                  :bitcoincharts-weighted-prices {:url "http://api.bitcoincharts.com/v1/weighted_prices.json" 
+                  :bitcoincharts-weighted-prices {:url "http://api.bitcoincharts.com/v1/weighted_prices.json"
                                            :method :get
                                            :limits "Max queries, once every 15 minutes"}
-                  :bitcoincharts-markets {:url "http://api.bitcoincharts.com/v1/markets.json" 
+                  :bitcoincharts-markets {:url "http://api.bitcoincharts.com/v1/markets.json"
                                            :method :get
                                            :limits "Max queries, once every 15 minutes"}}))
 
-(defn parse-numbers [m] 
-  (into {} 
+(defn parse-numbers [m]
+  (into {}
     (for [[k v] m] [k
-      (if (instance? java.util.Map v) 
+      (if (instance? java.util.Map v)
         (parse-numbers v)
         (if (or (nil? v) (number? v))
-          v 
+          v
           (if (and (instance? java.lang.String v) (boolean (re-matches #"^(\d*\.?\d*)$" v))) ; 2, 50, 0.001, 7.00091
-            (read-string v) 
-            v)))])))       
+            (read-string v)
+            v)))])))
 
 (defn parse-for [exchange ticker & [pair]]
-  (case exchange 
+  (case exchange
     :btce (:ticker ticker)
     :bter (parse-numbers (dissoc ticker :result))
     :bitstamp (parse-numbers ticker)
@@ -58,10 +58,10 @@
     :havelock (if (nil? pair)
                 (parse-numbers ticker)
                 ((keyword (upper-case pair)) (parse-numbers ticker)))
-    :bitcoincharts-weighted-prices (if-not (nil? pair) 
+    :bitcoincharts-weighted-prices (if-not (nil? pair)
                                      (parse-numbers (select-keys ticker [:timestamp (keyword (upper-case pair))]))
                                      (parse-numbers ticker))
-    :bitcoincharts-markets (if-not (nil? pair) 
+    :bitcoincharts-markets (if-not (nil? pair)
                              (first (filter #(= pair (:symbol %)) ticker))
                              ticker)
     nil))
@@ -77,27 +77,27 @@
     :bitcoincharts-markets (:url (exchange @feeds))
     nil))
 
-(defn http-options-for [exchange & [pair]] 
-  (case exchange 
+(defn http-options-for [exchange & [pair]]
+  (case exchange
     :bitstamp (assoc @default-options :pair "btc_usd" :exchange exchange :method (:method (exchange @feeds)))
     :havelock (if (nil? pair)
-                (assoc @default-options :pair pair :exchange exchange :method :get) 
+                (assoc @default-options :pair pair :exchange exchange :method :get)
                 (assoc @default-options :pair pair :exchange exchange :form-params {:symbol pair} :method :post))
     (assoc @default-options :pair pair :exchange exchange :method (:method (exchange @feeds)))))
 
 (defn callback [{:keys [status headers body error opts] :as response}]
-  (if error 
-    (throw (Exception. (format "Request failed, error: %s" error))) 
-    (case status 
+  (if error
+    (throw (Exception. (format "Request failed, error: %s" error)))
+    (case status
      200 (let [{:keys [pair exchange]} (:opts response)]
            (let [ticker (parse-string body true)]
              (if-not (nil? ticker) (parse-for exchange ticker pair))))
      (throw (Exception. (format "Request failed, response code: %s" status))))))
 
 (defn ticker [exchange & [pair]]
-  (if-not (contains? @feeds exchange) 
+  (if-not (contains? @feeds exchange)
     (throw (Exception. (format "Invalid exchange specified: %s" exchange)))
-    (if (and (:pair-required? (exchange @feeds)) (nil? pair)) 
+    (if (and (:pair-required? (exchange @feeds)) (nil? pair))
       (throw (Exception. (format "Currency pair must be specified for %s. Example: \"%s\"" exchange (:pair-example (exchange @feeds)))))
       (let [options (http-options-for exchange pair) url (url-for exchange pair)]
         (case (:method options)
